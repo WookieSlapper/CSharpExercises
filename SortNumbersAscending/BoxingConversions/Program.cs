9e5b864881733ebc80d31483a01d64881a16a2d1
Dominic Mazella <dominicmazella@gmail.com>
2014-05-20 20:36:35 -0400

    Conversions, Scope, Accessibility, Fields

---
+++ SortNumbersAscending/BoxingConversions/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxingConversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 36;
+            object boxed = i;
+
+            Console.WriteLine("i = {0}", i);
+            Console.WriteLine("boxed = {0}", boxed);
+
+            boxed = (int)boxed + 2;
+
+            Console.WriteLine("i = {0}", i);
+            Console.WriteLine("boxed = {0}", boxed);
+
+            i = i + 1;
+
+            Console.WriteLine("i = {0}", i);
+            Console.WriteLine("boxed = {0}", boxed);
+
+            i = (int) boxed;
+
+            Console.WriteLine("i = {0}", i);
+            Console.WriteLine("boxed = {0}", boxed);
+
+            int? h = null;
+            int? j = i;
+            object jboxed = j;
+
+            Console.WriteLine("h has a value? {0}", h.HasValue);
+
+            h = jboxed as int?;
+            
+            Console.WriteLine("h now has the value {0}:", h.Value);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ SortNumbersAscending/WorkingWithScope/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithScope
+{
+    class Contact
+    {
+        int age;
+        void F()
+        {
+            age = 18;
+        }
+
+        void G()
+        {
+            int age;
+            age = 21;
+            Console.WriteLine("The value of age is: {0}", age);
+        }
+
+        static void Main(string[] args)
+        {
+            Contact c = new Contact();
+            Console.WriteLine(c.age);
+            c.F();
+            Console.WriteLine(c.age);
+            c.G();
+            Console.WriteLine(c.age);
+
+            Console.ReadKey();
+        }
+    }
+}
